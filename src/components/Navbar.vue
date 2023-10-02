8990ccfb333b3ff77806f3c3de89b0a1fb53a651
nickcastela2022@hotmail.com <nickcastela2022@hotmail.com>
2023-10-02 10:03:33 -0400

    att-2

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav>
+            <router-link >
+                <img src=""  id="logo">
+            </router-link>
+         <router-link to="/">Home</router-link> |
+         <router-link to="/about">About</router-link>
+        </nav>
+    </div>
+</template>
+
+
+
+<script>
+
+export default{
+name:'Navbar'
+
+}
+</script>
+
