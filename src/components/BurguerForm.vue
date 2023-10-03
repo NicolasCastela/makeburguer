a28ba4c1d9828154df8e9094dc8a6d633ca44096
nickcastela2022@hotmail.com <nickcastela2022@hotmail.com>
2023-10-03 16:48:43 -0400

    #26

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>componente de menssagem</p>
         <div>
-            <form id="burguer-form">
+            <form id="burguer-form" @submit="createBurguer">
                 <div class="input-container">
                     <label for="nome">Nome do Cliente:</label>
                     <input type="text" name="nome" id="nome" v-model="nome" placeholder="Digite o seu nome!">
@@ -20,7 +20,7 @@
                     <label for="carne">Escolha a Carne</label>
                     <select name="carne" id="carne" v-model="carne">
                         <option value="">Selecione o tipo de carne</option>
-                        <option  v-for="carne in carnes" :key="carne.id" value="carne.tipo">{{ carne.tipo }}</option>
+                        <option  v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
                     </select>
                 </div>
                 <!-- || -->
@@ -66,8 +66,37 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+        },
+        async createBurguer(e) {
+        e.preventDefault();
+        
+        const data = { 
+            nome: this.nome,
+            carne: this.carne,
+            pao: this.pao,
+            opcionais: Array.from(this.opcionais),
+            status: 'Solicitado'
         }
+       const dataJson = JSON.stringify(data);
+
+        const req = await fetch("http://localhost:3000/burgers", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: dataJson
+        });
+         const res = await req.json();
+
+
+
+
+         this.pao = "";
+         this.carne = "";
+         this.nome = "";
+         this.opcionais = "";
+    }
     },
+
+    
     mounted() {
         this.getIngredientes()
     }
