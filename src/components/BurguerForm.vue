dcfaedaa46a61d75d027f3f3abc9e3b7618cff82
nickcastela2022@hotmail.com <nickcastela2022@hotmail.com>
2023-10-05 14:26:36 -0400

    melhoras na identação do codigo

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -1,6 +1,6 @@
 <template>
-    <div> 
-        <Message :msg="msg" v-show="msg"/>
+    <div>
+        <Message :msg="msg" v-show="msg" />
         <div>
             <form id="burguer-form" @submit="createBurger">
                 <div class="input-container">
@@ -20,18 +20,18 @@
                     <label for="carne">Escolha a Carne</label>
                     <select name="carne" id="carne" v-model="carne">
                         <option value="">Selecione o tipo de carne</option>
-                        <option  v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
+                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
                     </select>
                 </div>
                 <!-- || -->
                 <div id="opcionais-container" class="input-container">
                     <label id="opcionais-title" for="opcionais">Selecione os opcionais: </label>
-                    <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id" >
+                    <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
                         <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
-                        <span>{{opcional.tipo}}</span>
+                        <span>{{ opcional.tipo }}</span>
                     </div>
-                
-                    
+
+
                 </div>
                 <div class="input-container">
                     <input type="submit" class="submit-btn" value="criar meu burguer">
@@ -69,41 +69,41 @@ export default {
             this.opcionaisdata = data.opcionais;
         },
         async createBurger(e) {
-        e.preventDefault();
-        
-        const data = { 
-            nome: this.nome,
-            carne: this.carne,
-            pao: this.pao,
-            opcionais: Array.from(this.opcionais),
-            status: 'Solicitado'
-        }
-       const dataJson = JSON.stringify(data);
+            e.preventDefault();
 
-        const req = await fetch("http://localhost:3000/burgers", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: dataJson
-        });
-         const res = await req.json();
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: 'Solicitado'
+            }
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            });
+            const res = await req.json();
 
             this.msg = `Pedido N° ${res.id} realizado com sucesso! `;
 
-            setTimeout(() => this.msg = '', 5000 )
+            setTimeout(() => this.msg = '', 5000)
 
 
-         this.pao = "";
-         this.carne = "";
-         this.nome = "";
-         this.opcionais = "";
-    }
+            this.pao = "";
+            this.carne = "";
+            this.nome = "";
+            this.opcionais = "";
+        }
     },
 
-    
+
     mounted() {
         this.getIngredientes()
     },
-    components:{
+    components: {
         Message
     }
 }
